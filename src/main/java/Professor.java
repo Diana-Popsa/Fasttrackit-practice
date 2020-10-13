3392773c6014a10f91c6a9beed6b22ecb7558982
Diana <diana.popsa@yahoo.com>
2020-10-13 14:31:59 +0300

    done

---
+++ src/main/java/Professor.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Professor {
+    private String lastName;
+    private String firstName;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfHiring;
+    private String emailAddress;
+    private boolean isPHD;
+
+
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,64 @@
+import java.time.LocalDate;
+package ro.fasttrackit.classroom;
+
+public class Student {
+    private String lastName;
+    private String firstName;
+    private LocalDate dateOfBirth;
+    private String email;
+    private float Grade;
+    private boolean finished;
+
+    public Student(String givenLastName, LocalDate givenDateOfBirth) {
+        this.lastName = givenLastName;
+        this.dateOfBirth = givenDateOfBirth;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Float getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(Float grade) {
+        Grade = grade;
+    }
+
+    public Boolean getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Boolean finished) {
+        this.finished = finished;
+    }
+}
